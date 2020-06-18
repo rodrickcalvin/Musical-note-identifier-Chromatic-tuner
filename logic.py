5761f6304a0c790ef20fedfe936943afb9c67bd5
rodrickcalvin <rodrickwamala@gmail.com>
2020-06-18 17:58:43 +0300

    Cleans up the code

--- logic.py
+++ logic.py
@@ -8,47 +8,46 @@ import sys
 instrument = "Bass guitar"
 # =================================================================================================
 # The Note values are midi note Number
-# Advice to keep FRAME_SIZE and FRAMES_PER_FFT to be powers of two.
+# Advice to keep Frame_Size and Frames_per_FFT to be powers of two.
 class Tuner():
     def __init__(self, instrument):
         # BASS GUITAR
         if instrument == "Bass guitar":
-            self.NOTE_MIN = 23       # B0
-            self.NOTE_MAX = 48       # 
+            self.NOTE_MIN = 23              # B0
+            self.NOTE_MAX = 48              # C3
         
         # ACOUSTIC/ELECTRIC GUITAR
         if instrument == "Acoustic/Electric guitar":
-            self.NOTE_MIN = 40       # E2
-            self.NOTE_MAX = 64       # E4
+            self.NOTE_MIN = 40              # E2
+            self.NOTE_MAX = 64              # E4
         
         # UKELELE
         if instrument == "ukelele":
-            self.NOTE_MIN = 60       # C4
-            self.NOTE_MAX = 69       # A4
+            self.NOTE_MIN = 60              # C4
+            self.NOTE_MAX = 69              # A4
+
+        self.Sampling_Freq = 22050          # Sampling frequency in Hz
+        self.Frame_Size = 2048              # How many samples per frame?
+        # self.RECORDING_TIME = 3             # time to record audio
+        self.Frames_per_FFT = 16            # FFT takes average across how many frames?
+        self.Standard_Freq = 440.0
 
         # super().__init__()
         # self.ui = Ui_Dialog()
         # self.ui.setupUi(self)
 
 
-
 # create an instance
 t = Tuner(instrument)
 
-
-FSAMP = 22050      # Sampling frequency in Hz
-FRAME_SIZE = 2048   # How many samples per frame?
-RECORDING_TIME = 3  # time to record audio
-FRAMES_PER_FFT = 16 # FFT takes average across how many frames?
-
 ######################################################################
 # Derived quantities from constants above. Note that as
-# SAMPLES_PER_FFT goes up, the frequency step size decreases (so
+# Samples_per_FFT goes up, the frequency step size decreases (so
 # resolution increases); however, it will incur more delay to process
 # new sounds.
 
-SAMPLES_PER_FFT = FRAME_SIZE*FRAMES_PER_FFT
-FREQ_STEP = float(FSAMP)/SAMPLES_PER_FFT
+Samples_per_FFT = t.Frame_Size * t.Frames_per_FFT
+FREQ_STEP = float(t.Sampling_Freq)/Samples_per_FFT
 
 ######################################################################
 # For printing out notes
@@ -57,8 +56,8 @@ NOTE_NAMES = 'C C#/Db D D#/Eb E F F# G G#/Ab A A#/Bb B'.split()
 
 ######################################################################
 
-def freq_to_midi(f): return 69 + 12*np.log2(f/440.0)
-def midi_to_freq(n): return 440 * 2.0**((n-69)/12.0)
+def freq_to_midi(f): return 69 + 12*np.log2(f/t.Standard_Freq)
+def midi_to_freq(n): return int(t.Standard_Freq) * 2.0**((n-69)/12.0)
 def note_name(n): return NOTE_NAMES[n % 12] + str(n/12 - 1)
 
 ######################################################################
@@ -68,34 +67,34 @@ def note_name(n): return NOTE_NAMES[n % 12] + str(n/12 - 1)
 # See docs for numpy.rfftfreq()
 def note_to_fftbin(n): return midi_to_freq(n)/FREQ_STEP
 imin = max(0, int(np.floor(note_to_fftbin(t.NOTE_MIN-1))))
-imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(t.NOTE_MAX+1))))
+imax = min(Samples_per_FFT, int(np.ceil(note_to_fftbin(t.NOTE_MAX+1))))
 
 # Allocate space to run an FFT. 
-buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
+buf = np.zeros(Samples_per_FFT, dtype=np.float32)
 num_frames = 0
 
 # Initialize audio
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16,
                 channels=1,
-                rate=FSAMP,
+                rate=t.Sampling_Freq,
                 input=True,
-                frames_per_buffer=FRAME_SIZE)
+                frames_per_buffer=t.Frame_Size)
 
 stream.start_stream()
 
 # Create Hanning window function
-window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, SAMPLES_PER_FFT, False)))
+window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, Samples_per_FFT, False)))
 
 # Print initial text
-print('sampling at', FSAMP, 'Hz with max resolution of', FREQ_STEP, 'Hz')
+print('sampling at', t.Sampling_Freq, 'Hz with max resolution of', FREQ_STEP, 'Hz')
 
 # As long as we are getting data:
 while stream.is_active():
 
     # Shift the buffer down and new data in
-    buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
-    buf[-FRAME_SIZE:] = np.fromstring(stream.read(FRAME_SIZE), np.int16)
+    buf[:-t.Frame_Size] = buf[t.Frame_Size:]
+    buf[-t.Frame_Size:] = np.fromstring(stream.read(t.Frame_Size), np.int16)
 
     # Run the FFT on the windowed buffer
     fft = np.fft.rfft(buf * window)
@@ -110,6 +109,6 @@ while stream.is_active():
     # Console output once we have a full buffer
     num_frames += 1
 
-    if num_frames >= FRAMES_PER_FFT:
+    if num_frames >= t.Frames_per_FFT:
         print('freq: {:7.2f} Hz     note: {:>3s} {:+.2f}'.format(
             freq, note_name(n0), n-n0))
