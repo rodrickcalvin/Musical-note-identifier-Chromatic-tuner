fcb89f0fbba270565d1e4814d3a4978e0a713d5c
rodrickcalvin <rodrickwamala@gmail.com>
2020-11-09 22:06:32 +0300

    Updates xAudioControl

--- xAudioControl.py
+++ xAudioControl.py
@@ -4,16 +4,18 @@ import pyaudio
 
 class xAudio:
     def __init__(self):
-        xAudio.count = 0
-        xAudio.streamStatus = True
-        xAudio.NOTE_MIN = 60  # C4
-        xAudio.NOTE_MAX = 69  # A4
+        self.count = 0
+        xAudio.keyy= 0
+        xAudio.freqy = 0
+        xAudio.streamStatus = False
+        xAudio.NOTE_MIN = 0  # C4
+        xAudio.NOTE_MAX = 9000  # A4
         xAudio.FSAMP = 22050  # Sampling frequency in Hz
         xAudio.FRAME_SIZE = 2048  # How many samples per frame?
         xAudio.FRAMES_PER_FFT = 16  # FFT takes average across how many frames?
         xAudio.SAMPLES_PER_FFT = xAudio.FRAME_SIZE * xAudio.FRAMES_PER_FFT
         xAudio.FREQ_STEP = float(xAudio.FSAMP) / xAudio.SAMPLES_PER_FFT
-        xAudio.NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
+        xAudio.NOTE_NAMES = 'C C#/Db D D#/Eb E F F# G G#/Ab A Bb B'.split()
 
         xAudio.imin = max(0, int(np.floor(xAudio.note_to_fftbin(xAudio.NOTE_MIN - 1))))
         xAudio.imax = min(xAudio.SAMPLES_PER_FFT, int(np.ceil(xAudio.note_to_fftbin(xAudio.NOTE_MAX + 1))))
@@ -39,25 +41,37 @@ class xAudio:
 
     def note_to_fftbin(n):
         return xAudio.number_to_freq(n) / xAudio.FREQ_STEP
-    
-    def stopStream(self):
-        xAudio.streamStaus= False;
+
+    def setNoteMinMax(self, noteMin, noteMax):
+        xAudio.NOTE_MIN = noteMin
+        xAudio.NOTE_MAX = noteMax
+
+    def setStreamOff(self):
+        xAudio.streamStatus = False
+
+    def setStreamOn(self):
+        xAudio.streamStatus = True
+
+    def exitStream(self):
         xAudio.stream.stop_stream()
 
     def startStream(self):
-        xAudio.streamStaus = True;
-        # while True:
-        #     if (xAudio.streamStatus):
-        #         xAudio.buf[:-xAudio.FRAME_SIZE] = xAudio.buf[xAudio.FRAME_SIZE:]
-        #         xAudio.buf[-xAudio.FRAME_SIZE:] = np.fromstring(xAudio.stream.read(xAudio.FRAME_SIZE), np.int16)
-        #         xAudio.fft = np.fft.rfft(xAudio.buf * xAudio.window)
-        #         xAudio.freq = (np.abs(xAudio.fft[xAudio.imin:xAudio.imax]).argmax() + xAudio.imin) * xAudio.FREQ_STEP
-        #         xAudio.n = xAudio.freq_to_number(xAudio.freq)
-        #         xAudio.n0 = int(round(xAudio.n))
-        #         xAudio.num_frames += 1
-        #
-        #         if xAudio.num_frames >= xAudio.FRAMES_PER_FFT:
-        #             print('freq: {:7.2f} Hz     note: {:>3s} {:+.2f}'.format(xAudio.freq, xAudio.note_name(xAudio.n0), xAudio.n - xAudio.n0))
+        if (xAudio.streamStatus):
+            xAudio.buf[:-xAudio.FRAME_SIZE] = xAudio.buf[xAudio.FRAME_SIZE:]
+            xAudio.buf[-xAudio.FRAME_SIZE:] = np.fromstring(xAudio.stream.read(xAudio.FRAME_SIZE), np.int16)
+            xAudio.fft = np.fft.rfft(xAudio.buf * xAudio.window)
+            xAudio.freq = (np.abs(xAudio.fft[xAudio.imin:xAudio.imax]).argmax() + xAudio.imin) * xAudio.FREQ_STEP
+            xAudio.n = xAudio.freq_to_number(xAudio.freq)
+            xAudio.n0 = int(round(xAudio.n))
+            xAudio.num_frames += 1
 
-s = xAudio()
-s.startStream()
+            if xAudio.num_frames >= xAudio.FRAMES_PER_FFT:
+                print('freq: {:7.2f} Hz     note: {:>3s} {:+.2f}'.format(xAudio.freq, xAudio.note_name(xAudio.n0), xAudio.n - xAudio.n0))
+                print(str(xAudio.NOTE_MIN))
+                #xAudio.keyy= '{:>0.2s}'.format(xAudio.note_name(xAudio.n0))
+                #xAudio.freqy= '({:7.2f} Hz)'.format(xAudio.freq) +" Note Min "+str(xAudio.NOTE_MIN)+ " Note Max" +str(xAudio.NOTE_MAX)
+                xAudio.keyy= '{:>3s}'.format(xAudio.note_name(xAudio.n0))
+                xAudio.freqy= '({:7.2f} Hz)'.format(xAudio.freq) +" Note Min "+str(xAudio.NOTE_MIN)+ " Note Max" +str(xAudio.NOTE_MAX)
+
+# s = xAudio()
+#s.startStream()
